b8d2e6af2b4cf8d42bd55e419df535b4394f8f00
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-03-26 21:56:47 +0900

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 const MODE = Object.freeze({
   DEV: 'development',
@@ -19,21 +20,76 @@ module.exports = {
 
   module: {
     rules: [
-
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          scss: 'vue-style-loader!css-loader!sass-loader'
+        }
+      },
+      {
+        test: /\.tsx$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
     ]
   },
 
-  devServer: {
+  resolve: {
+    extensions: ['.ts', '.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
 
+  devServer: {
+    contentBase: 'dist',
+    historyApiFallback: true,
+    // noInfo: true,
+    // open: true
   },
 
   performance: {
-
+    hints: false
   },
 
   devtool: '#eval-source-map'
 }
 
-if (process.env.NODE_ENV === 'production') {
-  console.log('env: ', 'production')
+switch (process.env.NODE_ENV) {
+  case 'production': {
+    console.log('start production mode')
+    module.exports.mode = MODE.PROD
+    module.exports.devtool = '#source-map'
+    module.exports.optimization = {
+      minimize: true
+    }
+    module.exports.plugins = (module.exports.plugins || []).concat([
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"'
+        }
+      }),
+    ])
+    break
+  }
+
+  case 'development': {
+    console.log('start development mode')
+    break
+  }
+
+  default: {
+    break
+  }
 }
